827415a659e7b936f3a045012d7416d516ca86c3
puybr <56723522+puybr@users.noreply.github.com>
2024-11-08 23:47:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const { MongoClient } = require("mongodb");
 
 const express = require('express')
@@ -6,15 +8,13 @@ const port = 3000
 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded());
-require('dotenv').config()
+require('dotenv').config();
 
 const uri = process.env.SERVER_URL
 
 const client = new MongoClient(uri);
 
-
 async function main(){
-
     try{
         await client.connect();
         await getCollection(client)
@@ -24,7 +24,7 @@ async function main(){
     } finally {
         await client.close();
     }
-}
+};
 main().catch(console.error);
 
 
@@ -32,50 +32,50 @@ app.set('view engine', 'ejs');
 
 
 app.listen(port, () => {
-  console.log(`App listening at port ${port}`)
-})
+  console.log(`App listening at port ${port}`);
+});
 
 
 async function getCollection(client) {
     const myDB = client.db("messageboard");
-    const myColl = myDB.collection("messages")
+    const myColl = myDB.collection("messages");
     const cursor = myColl.find({});
     let messages = [
         {
           text: "Hi there!",
           user: "Amando",
-          added: new Date()
+          added: new Date();
         },
         {
           text: "Hello World!",
           user: "Charles",
-          added: new Date()
+          added: new Date();
         }
      ];
     
     for await (const doc of cursor) {
-        messages.push(doc)
+        messages.push(doc);
 
     app.get('/', (req, res) => {
         res.render('pages/index', {
             messages: messages
-            })
-    }) 
+            });
+    });
     app.post('/new', (req, res)=> { 
         const name = req.body.name
         const message = req.body.message
         const doc = {
             text: message,
             user: name,
-            added: new Date()
+            added: new Date();
           };
         if (doc.text&&doc.user) {
             client.connect();
   
           client.db("messageboard").collection("messages").insertOne(doc);
             messages.push(doc);
-            res.redirect('..')
+            res.redirect('..');
             } else return;      
-    })
-    }
-}
+      });
+    };
+ };
